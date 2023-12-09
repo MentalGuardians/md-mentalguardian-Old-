006fb0204398ec81fe774b4d09def44389d2a72d
Bangkit-Capstone-Project-CH2-PS001 <151497159+MentalGuardians@users.noreply.github.com>
2023-12-09 14:00:13 +0700

    Commit

---
+++ app/src/main/java/org/guardteam/mentalguardians/presentation/history/data/DataHistory.kt
@@ -0,0 +1,8 @@
+package org.guardteam.mentalguardians.presentation.history.data
+
+data class DataHistory(
+    val id: Long,
+    val date: String,
+    val time: String,
+    val diagnosa : String
+)
